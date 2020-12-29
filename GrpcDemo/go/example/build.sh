0295628a1440825ed85540169f42ecf3a3d30acf
cestlascorpion <cestlascorpion@gmail.com>
2020-12-29 14:18:57 +0800

    reserve for other demo

--- GrpcDemo/go/example/build.sh
+++ GrpcDemo/go/example/build.sh
@@ -1,6 +1,6 @@
 go mod tidy
 go mod download
 go mod vendor
-protoc -I=../proto --go_out=plugins=grpc:proto example.proto
+protoc -I=../../proto --go_out=plugins=grpc:proto example.proto
 go build -o build/example_server server/main.go
 go build -o build/example_client client/main.go
